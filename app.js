6c38848f6d6228a9500b2983aa13589ca57372f8
VarunBagul <102609054+VarunBagul@users.noreply.github.com>
2023-02-19 12:52:14 +0530

    cors implemented

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 // Requiring Modules
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 // Requiring files
-const userRouter = require('./routes/userRoutes');
-const viewRouter = require('./routes/viewRoutes');
+const userRouter = require("./routes/userRoutes");
+const viewRouter = require("./routes/viewRoutes");
 
-const AppError = require('./utils/AppError');
-const globalErrorHandler = require('./controllers/errorController');
+const AppError = require("./utils/AppError");
+const globalErrorHandler = require("./controllers/errorController");
 
 // Setting up app
 const app = express();
 
+// CORS
+app.use(cors());
+
 // Setting view engine
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 // Dotenv config path
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
 // Serving static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Using BodyParser to parse the body
@@ -40,11 +44,11 @@ app.use(
 // Middlewares
 
 // API Routes
-app.use('/', viewRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/", viewRouter);
+app.use("/api/v1/users", userRouter);
 
 // Not defined Routes
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(
     new AppError(
       `Can't find the requested url ${req.originalUrl} on this server!`,
